028f1d4214c961f5190d9c3103399de90d98bd84
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-31 21:37:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,3 +115,15 @@ def game_hash
     }
   }
 end
+
+def good_practices
+  game_hash.each do |location, team_data|
+    binding.pry
+      team_data.each do |attribute, data|
+        binding.pry
+          data.each do |data_item|
+            binding.pry
+          end
+        end
+      end
+    end
